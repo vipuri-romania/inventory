e8b9eff074f9503de1077ef595af46a3256a7f3f
adriantandara <adriantandara2005@yahoo.com>
2025-06-20 20:08:19 +0300

    fix: module for inventory data

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,15 @@
-declare const data: [
-  {
-    id: string;
-    name: string;
-    description: string;
-    image: string;
-    weight: number;
-    itemType: string;
-    isEquippable: boolean;
-    isStackable: boolean;
-    maxStack: number;
-  }
-];
+declare module "@vipuri/inventory_data" {
+  export const data: [
+    {
+      id: string;
+      name: string;
+      description: string;
+      image: string;
+      weight: number;
+      itemType: string;
+      isEquippable: boolean;
+      isStackable: boolean;
+      maxStack: number;
+    }
+  ];
+}
