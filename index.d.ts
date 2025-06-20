827a1fc7f566b8bcbd86a69ea3529008b96f4347
adriantandara <adriantandara2005@yahoo.com>
2025-06-20 20:05:37 +0300

    feat: add typescript support for inventory data

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare const data: [
+  {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    weight: number;
+    itemType: string;
+    isEquippable: boolean;
+    isStackable: boolean;
+    maxStack: number;
+  }
+];
