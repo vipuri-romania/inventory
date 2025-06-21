95bbf3d2154f8790a5a5e409554e40f64e47bd79
adriantandara <adriantandara2005@yahoo.com>
2025-06-21 09:07:08 +0300

    chore: removed old declarations from inventory data

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,5 @@
 import jsonData from "./data.json";
 
 declare module "@vipuri/inventory_data" {
-  export const data: [
-    {
-      id: string;
-      name: string;
-      description: string;
-      image: string;
-      weight: number;
-      itemType: string;
-      isEquippable: boolean;
-      isStackable: boolean;
-      maxStack: number;
-    }
-  ];
-
   export default jsonData;
 }
